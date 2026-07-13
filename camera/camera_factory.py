f88b1767b1b87f775aec1c5f7095d4807326e5dc
Nuruddeen <deenyala@gmail.com>
2026-07-13 07:42:00 +0700

    Delete camera_factory.py

--- camera/camera_factory.py
+++
@@ -1,6 +0,0 @@
-from camera.camera_device import CameraDevice
-
-class CameraFactory:
-    @staticmethod
-    def create_camera(cam_id: int, name: str, source: str) -> CameraDevice:
-        return CameraDevice(cam_id, name, source)
