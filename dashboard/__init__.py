9b8a7dd909231aef4a6fdefba9891667e9c5ba89
Nuruddeen <deenyala@gmail.com>
2026-07-15 01:01:37 +0700

    Delete __init__.py

--- dashboard/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dashboard import render_dashboard
-from .history import render_history
-from .analytics import render_analytics
-from .cameras import render_cameras
-from .settings import render_settings
